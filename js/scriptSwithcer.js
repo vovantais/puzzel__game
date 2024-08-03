1095acdf0621f807992439b7c15de7a0c50cdf36
uladzimir_hiadzeika@senla.eu <uladzimir_hiadzeika@senla.eu>
2024-08-03 13:33:59 +0200

    feat: add workflows

--- js/scriptSwithcer.js
+++ js/scriptSwithcer.js
@@ -1,12 +1,12 @@
-const select_feild = document.querySelector('.selection'),
-	choose_field_btn = document.querySelector('.choose-field'),
+const selectField = document.querySelector('.selection'),
+	chooseFieldBtn = document.querySelector('.choose-field'),
 	fieldSelection = document.getElementById('fieldSelection'),
 	dices = document.querySelectorAll('.dice'),
-	game_board = document.querySelector('.game-board');
+	gameBoard = document.querySelector('.game-board');
 size = localStorage.getItem("SIZE");
-choose_field_btn.addEventListener('click', (e) => {
+chooseFieldBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-	let valueOption = select_feild.options[select_feild.selectedIndex].value;
+	let valueOption = selectField.options[selectField.selectedIndex].value;
 	if (valueOption === "3x3" || valueOption === "4x4" || valueOption === "5x5") {
 		localStorage.setItem("SIZE", valueOption);
 		location.href = "";
@@ -16,21 +16,21 @@ if (size === "3x3") {
 	const script = document.createElement("script");
 	script.src = "./js/script3x3-min.js";
 	fieldSelection.append(script);
-	game_board.classList.add('small');
+	gameBoard.classList.add('small');
 	makeSmallField();
 } else if (size === "5x5") {
-	game_board.classList.remove('small')
+	gameBoard.classList.remove('small')
 	const script = document.createElement("script");
 	script.src = "./js/script5x5-min.js";
 	fieldSelection.append(script);
-	game_board.classList.add('big');
+	gameBoard.classList.add('big');
 	makeBigField();
 } else {
-	if (game_board.classList.contains('small') || game_board.classList.contains('big')) {
-		game_board.classList.remove('small');
-		game_board.classList.remove('big');
+	if (gameBoard.classList.contains('small') || gameBoard.classList.contains('big')) {
+		gameBoard.classList.remove('small');
+		gameBoard.classList.remove('big');
 	}
-	claerField();
+	clearField();
 	const script = document.createElement("script");
 	script.src = "./js/script4x4-min.js";
 	fieldSelection.append(script);
@@ -60,7 +60,7 @@ function makeBigField() {
 	}
 }
 
-function claerField() {
+function clearField() {
 	for (let i = 0; i < dices.length; i++) {
 		if (dices[i].classList.contains('field-small') || dices[i].classList.contains('hidden') || dices[i].classList.contains('field-big')) {
 			dices[i].classList.remove('field-small');
